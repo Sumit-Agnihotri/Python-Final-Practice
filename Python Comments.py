d5cb0ee067d2f7ce35be1007746c39ed5e89faf8
“[Sumit <sagnihotri9710@gmail.com>
2024-10-08 12:47:23 +0530

    Day  2

---
+++ Python Comments.py
@@ -0,0 +1,13 @@
+# This is a single-Line Comment
+
+
+'''This
+is
+a
+multi-line
+comment'''
+
+
+print("A \"COMMENT\" is a piece of code that is not executed. It is used to make the code more readable. We write them using the '#' symbol. We can also use multi-line comments using triple quotes.")
+
+print("Hello World")
