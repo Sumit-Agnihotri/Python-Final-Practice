eeeb2284eca200b751821df50776c9dcbc4ccc1d
“[Sumit <sagnihotri9710@gmail.com>
2024-10-09 23:24:33 +0530

    Day1

--- Python Data Types.py
+++ Python Data Types.py
@@ -1,2 +1,10 @@
-# Hello
-print("Hello")
+print("PYTHON DATA TYPES")
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
