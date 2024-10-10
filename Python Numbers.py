5c33c028e2fb3336008d16c16af3be251293c9e9
“[Sumit <sagnihotri9710@gmail.com>
2024-10-10 16:41:05 +0530

    Day 1

--- Python Numbers.py
+++ Python Numbers.py
@@ -9,3 +9,6 @@ print("w is of type:",type(w))
 print("x is of type:",type(x))
 print("y is of type:",type(y))
 print("z is of type:",type(z))
+
+print("INTEGER NUMBERS")
+print("Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.")
