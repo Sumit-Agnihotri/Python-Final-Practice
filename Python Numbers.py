b63d103aeada973e6445ea06dda79e33258a0a89
“[Sumit <sagnihotri9710@gmail.com>
2024-10-10 16:38:11 +0530

    Day1

--- Python Numbers.py
+++ Python Numbers.py
@@ -1,2 +1,9 @@
 # PYTHON NUMBERS
 print("There are 3 types of Python Numbers:\n1. int\n2. float\n3. complex")
+# For example:
+x=1
+y="Sumit"
+z=True
+print("x is of type:",type(x))
+print("y is of type:",type(y))
+print("z is of type:",type(z))
