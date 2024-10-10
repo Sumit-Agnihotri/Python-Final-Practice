b7365252ce816b2f9dbb5e21bffef01e4b64ea0a
“[Sumit <sagnihotri9710@gmail.com>
2024-10-10 16:57:43 +0530

    Energy

--- Python Numbers.py
+++ Python Numbers.py
@@ -55,3 +55,9 @@ print(j)
 i=3.4
 j=int(i)
 print(j)
+
+print("RANDOM NUMBER")
+print("Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers.")
+
+import random
+print(random.randrange(1,10)) # This will generate a random number between 1 and 9 (both included)
