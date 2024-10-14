186ae70437fc6c8484024349c5f4b43c316b48ce
“[Sumit <sagnihotri9710@gmail.com>
2024-10-14 21:24:00 +0530

    J

--- Python Casting.py
+++ Python Casting.py
@@ -9,3 +9,7 @@ x = float(1)     # x will be 1.0
 y = float(2.8)   # y will be 2.8
 z = float("3")   # z will be 3.0
 
+#Strings Example:
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
