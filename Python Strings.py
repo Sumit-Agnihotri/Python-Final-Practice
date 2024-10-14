8130578f5f3ac448df9e7963b981cd07951b1639
“[Sumit <sagnihotri9710@gmail.com>
2024-10-14 21:39:26 +0530

    Multi

--- Python Strings.py
+++ Python Strings.py
@@ -6,3 +6,5 @@ print('Hello') # Output: Hello
 print("It's alright!") # Output: It's alright!
 print("'Sumit' and 'Anshika' are friends") # It's a single quote inside double quotes.
 print('"Sumit" and "Anshika" are friends') # It's a double quote inside single quotes.
+
+# Multi-line Strings
