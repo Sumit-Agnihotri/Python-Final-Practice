d3820c19b3c06afaf502f4b0f6c01667ed1b0a18
“[Sumit <sagnihotri9710@gmail.com>
2024-10-14 21:22:49 +0530

    Om

--- Python Casting.py
+++ Python Casting.py
@@ -1,6 +1,5 @@
 print("Specify a Variable Type\nThere may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.\n\nCasting in python is therefore done using constructor functions:\n\n1-> int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)\n2-> float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)\n3-> str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals")
-x=1
-y=2
-x,y=y,x
-print(x)
-print(y)
+#Integers Example:
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
