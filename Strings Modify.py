52155545c0c24d0630b68a216945e0b667147fe2
“[Sumit <sagnihotri9710@gmail.com>
2024-10-16 19:28:43 +0530

    Modify

--- Strings Modify.py
+++ Strings Modify.py
@@ -1,7 +1,7 @@
 # Modify Strings
 a="   Hello, World!   "
-print(a.upper()) # It will convert all the characters in the string to upper case
-print(a.lower()) # It will convert all the characters in the string to lower case
-print(a.strip()) # It will remove any whitespace from the beginning or the end
-print(a.replace("H","J")) # It will replace the character "H" with "J"
-print(a.split(",")) # It will split the string into substrings if it finds instances of the separator
+print(a.upper()) # It will convert all the characters in the string to upper case.
+print(a.lower()) # It will convert all the characters in the string to lower case.
+print(a.strip()) # It will remove any whitespace from the beginning or the end.
+print(a.replace("H","J")) # It will replace the character "H" with "J".
+print(a.split(",")) # It will split the string into substrings if it finds instances of the separator.
