a6b8b29c3dc0c2f3c86ad75356bb4f61ac2e956d
“[Sumit <sagnihotri9710@gmail.com>
2024-10-16 22:01:23 +0530

    Concatenation

--- Strings Concatenation.py
+++ Strings Concatenation.py
@@ -1,3 +1,8 @@
 # Python String Concatenation
 # Concatenation is the process of combining two strings.
 # In Python, the '+' operator is used to concatenate strings.
+a="Hello"
+b="World"
+print(a+b) # Output: HelloWorld
+print(a,b) # Output: Hello World
+print(a+" "+b) # Output: Hello World
