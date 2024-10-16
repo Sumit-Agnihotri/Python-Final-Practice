f160ce6023ce9b98fccf6a3978dc63bb77216872
“[Sumit <sagnihotri9710@gmail.com>
2024-10-16 19:25:48 +0530

    M

--- Strings Modify.py
+++ Strings Modify.py
@@ -1,7 +1,7 @@
 # Modify Strings
 a="   Hello, World!   "
 print(a.upper()) # It will convert all the characters in the string to upper case
-print(a.lower())
-print(a.strip())
+print(a.lower()) # It will convert all the characters in the string to lower case
+print(a.strip()) 
 print(a.replace("H","J"))
 print(a.split(","))
