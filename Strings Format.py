8bfdd13c1bac8d916e752f8d21941ca1d35c4dfd
“[Sumit <sagnihotri9710@gmail.com>
2024-10-16 22:14:38 +0530

    F-Strings

--- Strings Format.py
+++ Strings Format.py
@@ -1,4 +1,11 @@
 age = 20
 name = "Sumit"
 print("My name is "+ name +" and I am "+ str(age) +" years old.")
-print("My name is",name,"and I am ",age,"years old.")
+print("My name is",name,"and I am ",age,"years old.\n")
+
+# F-Strings
+
+print("F-Strings\n")
+age = 20
+name = "Sumit"
+print(f"My name is {name} and I am {age} years old.")
