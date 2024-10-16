0927ff9e7ac77c0fef75d8df38eeae4fa77f719c
“[Sumit <sagnihotri9710@gmail.com>
2024-10-16 18:03:46 +0530

    Slice

---
+++ Strings Slicing.py
@@ -0,0 +1,12 @@
+# Slicing Strings
+# Slicing is a way to extract a subsequence of elements from a sequence.
+# In Python, we can slice strings using the following syntax:
+# Example:
+a=[1,2,3,4,5]
+print(a[1:3])
+# This is an example of String SLicing
+
+# Slice from Start
+# Example:
+a=[1,2,3,4,5]
+print(a[:3])
