03d65982589df7a2eff04e8e804a350459bf0c58
“[Sumit <sagnihotri9710@gmail.com>
2024-10-18 22:51:17 +0530

    Bool

--- Python Booleans.py
+++ Python Booleans.py
@@ -5,3 +5,11 @@
 # You can evaluate any expression in Python, and get one of two answers, True or False.
 print(10>9)
 print(10<9)
+print(10==9)
+# When you run a condition in an if statement, Python returns True or False:
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
