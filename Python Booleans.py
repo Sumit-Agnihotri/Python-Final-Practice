0bf9dd74ca23cd6fb606a215bfeb59df27111104
“[Sumit <sagnihotri9710@gmail.com>
2024-10-18 22:51:36 +0530

    Bool

--- Python Booleans.py
+++ Python Booleans.py
@@ -13,3 +13,9 @@ if b > a:
     print("b is greater than a")
 else:
     print("b is not greater than a")
+
+# Evaluate Values and Variables
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+# Example
+print(bool("Hello"))
+print(bool(15))
