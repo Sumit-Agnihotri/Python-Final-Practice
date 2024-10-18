6caa9b74141b4f61a0be801c0bbc4f678755b3a8
“[Sumit <sagnihotri9710@gmail.com>
2024-10-18 22:57:03 +0530

    Operator

---
+++ Python Operators.py
@@ -0,0 +1,10 @@
+# PYTHON OPERATORS
+# Operators are used to perform operations on variables and values.
+# Python divides the operators in the following groups:
+# 1:- Arithmetic operators
+# 2:- Assignment operators
+# 3:- Comparison operators
+# 4:- Logical operators
+# 5:- Identity operators
+# 6:- Membership operators
+# 7:- Bitwise operators
