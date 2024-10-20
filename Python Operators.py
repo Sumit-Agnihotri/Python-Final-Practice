f89df6037517a489b866fdb3917ed74686b6256a
“[Sumit <sagnihotri9710@gmail.com>
2024-10-20 16:43:50 +0530

    Operator

--- Python Operators.py
+++ Python Operators.py
@@ -11,7 +11,8 @@
 
 a=2
 b=3
-print("1:- Arithmetic operators")
+
+print("1:- Arithmetic Operators")
 print(a+b) # Addition
 print(a-b) # Subtraction
 print(a*b) # Multiplication
@@ -19,3 +20,27 @@ print(a/b) # Division
 print(a%b) # Modulus
 print(a**b) # Exponentiation
 print(a//b) # Floor division
+
+print("2:- Assignment Operators")
+a+=b
+print(a) # Add AND
+a-=b
+print(a) # Subtract AND
+a*=b
+print(a) # Multiply AND
+a/=b
+print(a) # Divide AND
+a%=b
+print(a) # Modulus AND
+a**=b
+print(a) # Exponentiation AND
+a//=b
+print(a) # Floor division AND
+
+print("3:- Comparison Operators")
+print(a==b) # Equal
+print(a!=b) # Not equal
+print(a>b) # Greater than
+print(a<b) # Less than
+print(a>=b) # Greater than or equal to
+print(a<=b) # Less than or equal to
