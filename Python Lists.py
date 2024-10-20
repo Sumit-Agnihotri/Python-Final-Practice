7a71186f187db0a0ab4a3dda6b6f029fa5c886de
“[Sumit <sagnihotri9710@gmail.com>
2024-10-20 18:02:35 +0530

    List

---
+++ Python Lists.py
@@ -0,0 +1,4 @@
+# PYTHON LISTS
+print("Lists are used to store multiple items in a single variable.")
+print("Lists are one of 4 built-in data types in Python used to store collections of data\n, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.")
+# Lists are mutable, meaning that the elements inside a list can be changed!
