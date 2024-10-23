48e9d114b53313343a4631d63965b44e3e22b7a8
“[Sumit <sagnihotri9710@gmail.com>
2024-10-23 23:46:17 +0530

    List

--- List Access Items.py
+++ List Access Items.py
@@ -1,1 +1,4 @@
-# Python List Access Items
+# PYTHON LIST ACCESS ITEMS
+# List items are indexed and you can access them by referring to the index number.
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) # banana
