3f3821f5f2324ba1e193c73451bfbc384ab5c438
“[Sumit <sagnihotri9710@gmail.com>
2024-10-23 23:47:20 +0530

    List

--- List Access Items.py
+++ List Access Items.py
@@ -2,3 +2,8 @@
 # List items are indexed and you can access them by referring to the index number.
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1]) # banana
+
+# Negative Indexing
+# Negative indexing means start from the end
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1]) # cherry
