d6aa2bf16db642afa060ca8fa1f256840010d8c7
“[Sumit <sagnihotri9710@gmail.com>
2024-10-23 23:48:54 +0530

    List

--- List Access Items.py
+++ List Access Items.py
@@ -7,3 +7,15 @@ print(thislist[1]) # banana
 # Negative indexing means start from the end
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1]) # cherry
+
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) # ['cherry', 'orange', 'kiwi']
+
+
+
+
+git add .
+git commit -m "List"
+git push
